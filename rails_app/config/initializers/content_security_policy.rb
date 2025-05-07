e5aa8df280de59185ae613c5a19fd93b78d02870
Matt Taylor <mattius.taylor@gmail.com>
2025-05-06 21:44:25 -0700

    skip coors checking since this is an API

--- rails_app/config/initializers/content_security_policy.rb
+++ rails_app/config/initializers/content_security_policy.rb
@@ -4,22 +4,13 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'  # Replace '*' with frontend domain (e.g., 'http://localhost:19006' for Expo)
+    resource '*',
+      headers: :any,
+      expose: ['Authorization'], # Exposes JWT token in responses if needed
+      methods: [:get, :post, :put, :patch, :delete, :head]
+  end
+end
+
